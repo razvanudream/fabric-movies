28d15418e0ae7431eef4002c67c63ec902b7edc7
Razvan Udrea <razvanudream@gmail.com>
2023-01-25 14:05:55 +0200

    Added Bull MQ for inserts into mongodb during search

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ import express from "express";
 import parser from "body-parser";
 import { graphqlHTTP } from "express-graphql";
 import mongoose from "mongoose";
-
+import Arena from "bull-arena";
+import { createBullBoard } from "@bull-board/api";
+import { ExpressAdapter, BullMQAdapter } from "@bull-board/express";
+import Image from "./models/image.js";
+import Movie from "./models/movie.js";
 import graphQLSchema from "./graphql/schema/index.js";
 import rootResolvers from "./graphql/resolvers/index.js";
+import { Queue, Worker } from "bullmq";
 
 const app = express();
 
@@ -31,6 +36,76 @@ app.use(
   })
 );
 
+const redisOptions = { port: 6379, host: "127.0.0.1" };
+
+const queues = {
+  movieQueue: new Queue("movie_queue", {
+    connection: redisOptions,
+  }),
+};
+
+const schedulers = new Worker(queues.movieQueue.name, async (job) => {
+  const movie = job.data;
+
+  try {
+    const posterRecord = new Image({
+      Poster: movie.Poster,
+    });
+
+    const movieRecord = new Movie({
+      Title: movie.Title,
+      Year: movie.Year,
+      imdbID: movie.imdbID,
+      Type: movie.Type,
+      Image: posterRecord,
+    });
+
+    await movieRecord.save();
+    await posterRecord.save();
+  } catch (err) {
+    /**
+     * It's most likely a duplicate key due to unique index
+     * on movie collection which is why we save the movie first
+     * then the poster for it
+     */
+  }
+});
+
+const arena = Arena(
+  {
+    BullMQ: Queue,
+    queues: [
+      {
+        type: "bullmq",
+        name: queues.movieQueue.name,
+        hostId: "worker",
+        redis: redisOptions,
+      },
+    ],
+  },
+  { disableListen: true }
+);
+
+app.use("/arena", arena);
+
+const PORT = process.env.PORT || 3010;
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+  console.log(`Bull arena is available at: http://localhost:${PORT}/arena`);
+});
+
+const serverAdapter = new ExpressAdapter();
+
+createBullBoard({
+  queues: [new BullMQAdapter(queues.movieQueue)],
+  serverAdapter: serverAdapter,
+});
+
+serverAdapter.setBasePath("/bull-board");
+
+app.use("/bull-board", serverAdapter.getRouter());
+
 mongoose.set("strictQuery", false);
 
 /**
@@ -40,6 +115,7 @@ mongoose.set("strictQuery", false);
  * and only to a certain folder where your credentials are
  * stored
  */
+
 mongoose
   .connect(
     `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@fabric.avfqbqt.mongodb.net/${process.env.MONGODB_NAME}?retryWrites=true&w=majority`
--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,8 +1,7 @@
 import cachios from "cachios";
-import Image from "../../models/image.js";
-import Movie from "../../models/movie.js";
 import { formatError } from "../../utils/formatError.js";
 import LRU from "lru-cache";
+import Bull from "bull";
 
 const CACHE_TTL_IN_SECONDS = 6 * 60 * 60; // 6 hours
 
@@ -25,40 +24,23 @@ const rootResolvers = {
         }`
       );
 
+      const queue = new Bull("movie_queue", {
+        redis: process.env.REDIS_URL,
+      });
+
       const movies = {
         results: response?.data?.Search,
         errors: [],
       };
 
-      /**
-       * Since there are only 10 records per page for omdbapi
-       * which cannot be changed, this won't impact loading times,
-       * however, a queue system (e.g. AWS SQS) would be ideal here
-       * to delegate this task to and return results faster to FE.
-       */
       movies.results.forEach(async (movie) => {
-        try {
-          const posterRecord = new Image({
-            Poster: movie.Poster,
-          });
-
-          const movieRecord = new Movie({
-            Title: movie.Title,
-            Year: movie.Year,
-            imdbID: movie.imdbID,
-            Type: movie.Type,
-            Image: posterRecord,
-          });
-
-          await movieRecord.save();
-          await posterRecord.save();
-        } catch (err) {
-          /**
-           * It's most likely a duplicate key due to unique index
-           * on movie collection which is why we save the movie first
-           * then the poster for it
-           */
-        }
+        await queue.add(movie, {
+          attempts: 3,
+          backoff: {
+            type: "exponential",
+            delay: 5000,
+          },
+        });
       });
 
       return movies;
